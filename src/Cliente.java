83f2489e7ff8631a0460bdfec229e2d068db1dc7
Endermaiter <mpereznovoa@danielcastelao.org>
2023-02-10 20:05:46 +0100

    Codigo Pulido. Problema de conexion Servidor-Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -20,26 +20,44 @@ public class Cliente {
 
             switch (opcion) {
                 case 1, 2, 3, 4 -> {
-                    String dato = JOptionPane.showInputDialog(null, "Dime un número:", "CLIENTE", JOptionPane.QUESTION_MESSAGE);
-                    String numeroDigitosDato = String.valueOf(dato.length());
 
+                    //**ENVIAR DATO**
+
+                    //Recogida de dato en variable
+                    String dato = JOptionPane.showInputDialog(null, "Dime un número:", "CLIENTE", JOptionPane.QUESTION_MESSAGE);
+                    //Recogida del temaño del dato en variable
+                    String numeroDigitosDato = String.valueOf(dato.length());
+                    //Enviamos al metodo enviarDatos() el dato, la opcion escogida y el tamaño del dato
                     enviarDatos(dato, String.valueOf(opcion), numeroDigitosDato);
 
+                    //**DEVOLUCION DEL DATO**
+
+                    //Creamos un nuevo socket
                     ServerSocket clientSocket = new ServerSocket();
+
+                    //Realizamos el bind
                     InetSocketAddress addr = new InetSocketAddress("localhost", 5566);
                     clientSocket.bind(addr);
+
+                    //Esperamos a que llegue el dato
                     Socket newSocket = clientSocket.accept();
+
+                    //Instanciamos el objeto de entrada
                     InputStream is = newSocket.getInputStream();
+
+                    //Metemos el dato recibido en una variable
                     byte[] arrayDatoDevuelto = new byte[20];
                     is.read(arrayDatoDevuelto);
+                    String datoDevuelto = new String(arrayDatoDevuelto);
 
+                    //diferentes mensajes en funcion de la opcion escogida por el cliente
                     switch (opcion) {
-                        case 1 -> JOptionPane.showMessageDialog(null, "Eso son " + new String(arrayDatoDevuelto) + " campos de fútbol");
-                        case 2 -> JOptionPane.showMessageDialog(null, "Te quedan " + new String(arrayDatoDevuelto) + " meses para la jubilación con paga completa");
-                        case 3 -> JOptionPane.showMessageDialog(null, "Eso son " + new String(arrayDatoDevuelto) + " obras de Pérez Reverte");
-                        case 4 -> JOptionPane.showMessageDialog(null, "Eso son " + new String(arrayDatoDevuelto) + "€/L mas cara que la gasolinera mas barata de la zona");
+                        case 1 -> JOptionPane.showMessageDialog(null, "Eso son " + datoDevuelto + " campos de fútbol");
+                        case 2 -> JOptionPane.showMessageDialog(null, "Te quedan " + datoDevuelto + " meses para la jubilación con paga completa");
+                        case 3 -> JOptionPane.showMessageDialog(null, "Eso son " + datoDevuelto + " obras de Pérez Reverte");
+                        case 4 -> JOptionPane.showMessageDialog(null, "Eso son " + datoDevuelto + "€/L mas cara que la gasolinera mas barata de la zona");
                     }
-
+                    //Una vez mostrado el dato junto al mensaje, ceramos los sockets
                     newSocket.close();
                     clientSocket.close();
                 }
@@ -52,33 +70,46 @@ public class Cliente {
 
     }
 
-    public static void enviarDatos(String dato, String opcion, String numeroDigitosDato) {
-        try {
-            System.out.println("Creando socket cliente");
-            Socket clienteSocket = new Socket();
-            System.out.println("Estableciendo la conexión");
+    // Método para enviar un dato determinado al servidor
+    public static void enviarDatos(String dato, String opcion, String numeroDigitosDato) throws IOException {
 
-            InetSocketAddress addr = new InetSocketAddress("localhost", 5555);
-            clienteSocket.connect(addr);
+        //Creando socket de cliente
+        Socket newSocket = new Socket();
+        System.out.println("Estableciendo la conexión con el servidor...\n");
 
-            OutputStream os = clienteSocket.getOutputStream();
+        //Asignando direccion ip y puerto
+        InetSocketAddress addr = new InetSocketAddress("localhost", 5555);
+        newSocket.connect(addr);
 
-            System.out.println("Enviando dato");
+        //Instancia del objeto de salida
+        OutputStream os = newSocket.getOutputStream();
+        System.out.println("¡¡Conexión aceptada!!\n");
 
+        //Condicionante que determina si quiero enviar un dato al servidor o cerrarlo
+        if (opcion.equals("5")) {
+
+            //Envia instruccion de que se cierre el servidor
+            System.out.println("Cerrando servidor...\n");
             os.write(opcion.getBytes());
             os.write(numeroDigitosDato.getBytes());
-            os.write(dato.getBytes());
 
-            System.out.println("Dato enviado");
+            //Cerrando socket del cliente(puerto:5555)
+            newSocket.close();
 
-            System.out.println("Cerrando el socket cliente");
+            System.out.println("**Servidor cerrado**");
 
-            clienteSocket.close();
+        } else {
 
-            System.out.println("Terminado");
+            //Enviando datos
+            System.out.println("Enviando datos...\n");
+            os.write(opcion.getBytes());                //Numero de instrucción
+            os.write(numeroDigitosDato.getBytes());     //Numero de dígitos del dato
+            os.write(dato.getBytes());                  //Dato
+            System.out.println("¡¡Datos enviados!!");
 
-        } catch (IOException e) {
-            e.printStackTrace();
+            //Cerrando socket del cliente (port:5555)
+            newSocket.close();
         }
+
     }
 }
