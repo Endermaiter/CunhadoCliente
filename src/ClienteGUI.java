82567d9e2b537a54a8486735b642ec508f296f69
Endermaiter <mpereznovoa@danielcastelao.org>
2023-02-14 11:35:47 +0100

    Creacion de la interfaz grafica(Botones y labels)

---
+++ src/ClienteGUI.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class ClienteGUI {
+    private JButton futbolButton;
+    private JButton jubilacionButton;
+    private JButton reverteButton;
+    private JButton gasolinaButton;
+    private JButton saírButton;
+    private JLabel separator1;
+    private JLabel separator2;
+    private JLabel separator3;
+    private JLabel separator4;
+}
