235f73d2782d938787874519a387a95e027aaf4b
Endermaiter <mpereznovoa@danielcastelao.org>
2023-02-14 11:51:06 +0100

    Eventos de boton hechos

--- src/ClienteGUI.java
+++ src/ClienteGUI.java
@@ -1,13 +1,34 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ClienteGUI {
     private JButton futbolButton;
     private JButton jubilacionButton;
     private JButton reverteButton;
     private JButton gasolinaButton;
-    private JButton saírButton;
+    private JButton sairButton;
     private JLabel separator1;
     private JLabel separator2;
     private JLabel separator3;
     private JLabel separator4;
+
+
+public ClienteGUI() {
+    futbolButton.addActionListener(e -> {
+
+    });
+    jubilacionButton.addActionListener(e -> {
+
+    });
+    reverteButton.addActionListener(e -> {
+
+    });
+    gasolinaButton.addActionListener(e -> {
+
+    });
+    sairButton.addActionListener(e -> {
+
+    });
+}
 }
